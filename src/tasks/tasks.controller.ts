1468c6077b018bc945047b0909f4ef137ea29e9d
uniondeveloper2021 <86847362+uniondeveloper2021@users.noreply.github.com>
2022-03-18 09:10:39 -0500

    add task module and service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Request, Response } from 'express';
 @Controller('tasks')
 export class TasksController {
   @Get()
-  getTasks(): string {
-    return 'Tasks';
+  getTasks(): { message: string } {
+    return { message: 'Get task' };
+  }
+
+  @Post()
+  addTask(@Body() task: CreateTaskDto): string {
+    console.log(task.title);
+    console.log(task.descripcion);
+    console.log(task.done);
+    console.log(task);
+    return 'add Tasks';
+  }
+
+  @Put(':taskId')
+  putTask(@Body() task: CreateTaskDto, @Param('taskId') taskId): { data: {}, message: string, statusCode: number } {
+    return { data: { taskId, data: task }, message: 'Task updated', statusCode: 200 };
+  }
+
+  @Delete(':taskId')
+  deleteTask(@Param('taskId') taskId): { data: {}, message: string, statusCode: number } {
+    console.log(taskId);
+    return { data: { taskId }, message: 'Task deleted', statusCode: 200 };
   }
 }
